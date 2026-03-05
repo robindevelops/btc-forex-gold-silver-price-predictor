4ef98e496f11fbe73ffa88296861cc21699c95d4
Alyan Shahid <alyanshahidsdk@gmail.com>
2026-03-05 11:24:52 +0500

    feat: Replace static variable comparison with an interactive arithmetic calculator.

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-a  = 2
+first_number  = int(input("Enter a number: "))
+second_number = int(input("Enter a number: "))
+arithmetic_operation = input("Enter an arithmetic operation: ")
 
-b = 4
+
+if (arithmetic_operation == "+"):
+    print(first_number + second_number)
+elif (arithmetic_operation == "-"):
+    print(first_number - second_number)
+elif (arithmetic_operation == "*"):
+    print(first_number * second_number)
+elif (arithmetic_operation == "/"):
+    print(first_number / second_number)
+else:
+    print("Invalid operation")
 
 
-print(a > b)
 
